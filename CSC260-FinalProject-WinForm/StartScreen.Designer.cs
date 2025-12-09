915b19fd3d9ef2d844767f6899d3d4a02322d5e0
CorbBauc <corbinmbaucom@gmail.com>
2025-12-09 14:43:12 -0600

    Created Start Screen user control that includes a start button. This start button applies the GameScreen user control to the Panel on the MainForm.

---
+++ CSC260-FinalProject-WinForm/StartScreen.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace CSC260_FinalProject_WinForm
+{
+    partial class StartScreen
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(213, 168);
+            button1.Name = "button1";
+            button1.Size = new Size(236, 200);
+            button1.TabIndex = 0;
+            button1.Text = "START";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // StartScreen
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDark;
+            Controls.Add(button1);
+            Name = "StartScreen";
+            Size = new Size(710, 584);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+    }
+}
---
+++ CSC260-FinalProject-WinForm/StartScreen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSC260_FinalProject_WinForm
+{
+    public partial class StartScreen : UserControl
+    {
+        public StartScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ((MainForm)this.FindForm()).ShowGameScreen();
+        }
+    }
+}
