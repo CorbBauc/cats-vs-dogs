e3f2d73835c4250ba9c000e860492b3363e849f6
CorbBauc <corbinmbaucom@gmail.com>
2025-12-09 20:32:34 -0600

    Created the abstract Card which inlcudes three attributes, a parameterized constructor, and five methods. One of which is virtual allowing for method overriding.

---
+++ CSC260-FinalProject-WinForm/Card.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC260_FinalProject_WinForm
+{
+    public abstract class Card
+    {
+        protected int health;
+        protected int damage;
+        protected int manaCost;
+
+        public Card(int health, int damage, int manaCost)
+        {
+            this.health = health;
+            this.damage = damage;
+            this.manaCost = manaCost;
+        }
+
+        protected virtual void useSpecialAbility()
+        {
+
+        }
+
+        protected void TakeCardDamage()
+        {
+
+        }
+
+        protected void IncreaseCardDamage()
+        {
+
+        }
+
+        protected void IncreaseCardHealth()
+        {
+
+        }
+
+        protected void DecreaseManaCost()
+        {
+
+        }
+
+    }
+}
