f7761ffb82492619d5250457db0ebabf6ce17831
CorbBauc <corbinmbaucom@gmail.com>
2025-12-09 20:24:01 -0600

    Created top level Game class which currently incldues one attribute, one default constructor, and four methods

---
+++ CSC260-FinalProject-WinForm/Game.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC260_FinalProject_WinForm
+{
+    public class Game
+    {
+        private int _gameTimer = 10000;
+
+        public Game()
+        {
+
+        }
+        public void startGame()
+        {
+
+        }
+
+        public void endGame()
+        {
+
+        }
+
+        public void startTurn() 
+        { 
+
+        }
+
+        public void endTurn()
+        {
+
+        }
+
+
+    }
+}
