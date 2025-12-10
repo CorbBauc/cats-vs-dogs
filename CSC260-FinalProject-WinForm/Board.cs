1becb216b19948e1f58f9e9abc948764260ecb8d
CorbBauc <corbinmbaucom@gmail.com>
2025-12-09 20:24:46 -0600

    Created Board class which incldues two attributes, a default constructor, and two methods.

---
+++ CSC260-FinalProject-WinForm/Board.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC260_FinalProject_WinForm
+{
+    internal class Board
+    {
+        private List<Card> _p1ActiveCards;
+        private List<Card> _p2ActiveCards;
+
+        public Board()
+        {
+
+        }
+
+        public void placeCard(Card card)
+        {
+
+        }
+        public void destroyCard(Card card)
+        {
+
+        }
+    }
+}
