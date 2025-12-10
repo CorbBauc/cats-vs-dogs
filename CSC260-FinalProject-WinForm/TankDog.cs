75f321e3bb5350a8509a95b2d7c85526a254d269
CorbBauc <corbinmbaucom@gmail.com>
2025-12-09 20:34:24 -0600

    Created TankDog class which is an individual card that has one default constructor that passes hard coded values through up to its parent class.

---
+++ CSC260-FinalProject-WinForm/TankDog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC260_FinalProject_WinForm
+{
+    public class TankDog : Dog
+    {
+        public TankDog() 
+            : base (health: 10, damage: 3, manaCost: 5) 
+        {
+
+        }
+    }
+}
