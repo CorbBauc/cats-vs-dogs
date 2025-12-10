7364751446fcc97dfe1673751347119d069b658c
CorbBauc <corbinmbaucom@gmail.com>
2025-12-09 20:35:37 -0600

    Created Player class that includes four attributes, one paramterized constructor, and three methods.

---
+++ CSC260-FinalProject-WinForm/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC260_FinalProject_WinForm
+{
+    internal class Player
+    {
+        private string _chosenTeam;
+        private int _health;
+        private int _mana;
+        public List<Card> hand;
+
+        public Player(string team)
+        {
+            _chosenTeam = team;
+        }
+
+        public void takeDamage(int damage)
+        {
+
+        }
+
+        public void consumeMana(int mana)
+        {
+
+        }
+
+        public void playCard(Card card)
+        {
+
+        }
+    }
+}
