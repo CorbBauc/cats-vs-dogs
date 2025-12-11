1efd34b7bc3f7facdf28edc238f8a77632c3de35
CorbBauc <corbinmbaucom@gmail.com>
2025-12-11 12:52:24 -0600

    Created SpecialDog class for cards that will have special abilities as those will have to be classes in and of themselves to override the useSpecialAbility method.

---
+++ CSC260-FinalProject-WinForm/SpecialDog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC260_FinalProject_WinForm
+{
+    public abstract class SpecialDog : Card
+    {
+        SpecialDog(string cardName, int health, int damage, int manaCost) 
+            : base(cardName, health, damage, manaCost)
+        {
+
+        }
+
+    }
+}
