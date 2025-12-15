1fc0d1890f0d11e84ac616a2c7a4e0f4488c2ef8
CorbBauc <corbinmbaucom@gmail.com>
2025-12-15 17:20:32 -0600

    Created BoardSlot UserControl for event logic uses.
    Includes three fields, Card _card, bool _isEmpty, event Action<BoardSlot> SlotClicked
    
    The constructor adds the UI elements Clicks to the BoardSlot_Click function so that the function is called when each UI item is clicked.
    
    There is a public property, IsEmpty that returns isEmpty
    
    There are three total methods.
    The first LoadCardInSlot, takes a Card object, sets that card object to the BoardSlots local Card object and passes the Cards data from one object to the other. isEmpty is also set to false when this is called.
    
    The second method, ClearSlot, sets the Card object to null, isEmpty to true, and clears the data out of the UI elements.
    
    The final method is an event method that invokes when the Slot is clicked. And sends the event to any objects added to the SlotClicked event.

---
+++ CSC260-FinalProject-WinForm/BoardSlot.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace CSC260_FinalProject_WinForm
+{
+    partial class BoardSlot
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBoxCardArt = new PictureBox();
+            labelMana = new Label();
+            labelHealth = new Label();
+            labelDamage = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxCardArt).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBoxCardArt
+            // 
+            pictureBoxCardArt.Location = new Point(0, 0);
+            pictureBoxCardArt.Name = "pictureBoxCardArt";
+            pictureBoxCardArt.Size = new Size(115, 150);
+            pictureBoxCardArt.TabIndex = 0;
+            pictureBoxCardArt.TabStop = false;
+            // 
+            // labelMana
+            // 
+            labelMana.AutoSize = true;
+            labelMana.Location = new Point(65, 11);
+            labelMana.Name = "labelMana";
+            labelMana.Size = new Size(38, 15);
+            labelMana.TabIndex = 2;
+            labelMana.Text = "label1";
+            // 
+            // labelHealth
+            // 
+            labelHealth.AutoSize = true;
+            labelHealth.Location = new Point(3, 123);
+            labelHealth.Name = "labelHealth";
+            labelHealth.Size = new Size(38, 15);
+            labelHealth.TabIndex = 4;
+            labelHealth.Text = "label2";
+            // 
+            // labelDamage
+            // 
+            labelDamage.AutoSize = true;
+            labelDamage.Location = new Point(65, 123);
+            labelDamage.Name = "labelDamage";
+            labelDamage.Size = new Size(38, 15);
+            labelDamage.TabIndex = 6;
+            labelDamage.Text = "label3";
+            // 
+            // BoardSlot
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(labelHealth);
+            Controls.Add(labelDamage);
+            Controls.Add(labelMana);
+            Controls.Add(pictureBoxCardArt);
+            Name = "BoardSlot";
+            Size = new Size(115, 150);
+            ((System.ComponentModel.ISupportInitialize)pictureBoxCardArt).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBoxCardArt;
+        private Label labelMana;
+        private Label labelHealth;
+        private Label labelDamage;
+    }
+}
---
+++ CSC260-FinalProject-WinForm/BoardSlot.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSC260_FinalProject_WinForm
+{
+    public partial class BoardSlot : UserControl
+    {
+        private Card _card;
+        private bool _isEmpty = true;
+        public event Action<BoardSlot> SlotClicked;
+        public BoardSlot()
+        {
+            InitializeComponent();
+            this.Click += BoardSlot_Click;
+            pictureBoxCardArt.Click += BoardSlot_Click;
+            labelHealth.Click += BoardSlot_Click;
+            labelMana.Click += BoardSlot_Click;
+            labelDamage.Click += BoardSlot_Click;
+
+        }
+
+        public bool IsEmpty
+        {
+            get { return _isEmpty; }
+        }
+
+        public void LoadCardInSlot(Card card)
+        {
+            _card = card;
+
+            if(card == null)
+            {
+                ClearSlot();
+                return;
+            }
+
+            _isEmpty = false;
+            pictureBoxCardArt.Image = card.Art;
+            labelHealth.Text = card.Health.ToString();
+            labelMana.Text = card.Mana.ToString();
+            labelDamage.Text = card.Damage.ToString();
+        }
+
+        public void ClearSlot()
+        {
+            _card = null;
+            _isEmpty = true;
+
+            pictureBoxCardArt.Image = null;
+            labelHealth.Text = "";
+            labelMana.Text = "";
+            labelDamage.Text = "";
+        }
+
+        public void BoardSlot_Click(object sender, EventArgs e)
+        {
+            SlotClicked?.Invoke(this);
+        }
+    }
+}
