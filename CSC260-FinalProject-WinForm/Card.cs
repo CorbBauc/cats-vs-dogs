6500244a1eca65937dafa2387eca68235187755b
CorbBauc <corbinmbaucom@gmail.com>
2025-12-15 16:03:07 -0600

    Added art field that is type Image. Added public getters and setters for all protected fields. Additionally, I added the Image art in the parameterized constructor.

--- CSC260-FinalProject-WinForm/Card.cs
+++ CSC260-FinalProject-WinForm/Card.cs
@@ -12,13 +12,39 @@ namespace CSC260_FinalProject_WinForm
         protected int health;
         protected int damage;
         protected int manaCost;
+        protected Image art;
 
-        public Card(string cardName, int health, int damage, int manaCost)
+        public Card(string cardName, int health, int damage, int manaCost,Image art)
         {
             this.cardName = cardName;
             this.health = health;
             this.damage = damage;
             this.manaCost = manaCost;
+            this.art = art;
+        }
+
+        public int Health
+        {
+            get {  return health; }
+            set { health = value; }
+        }
+
+        public int Damage
+        {
+            get { return damage; }
+            set { damage = value; }
+        }
+
+        public int Mana
+        {
+            get { return manaCost; }
+            set { manaCost = value; }
+        }
+
+        public Image Art
+        {
+            get { return art; }
+            set { art = value; }
         }
 
         protected virtual void useSpecialAbility()
