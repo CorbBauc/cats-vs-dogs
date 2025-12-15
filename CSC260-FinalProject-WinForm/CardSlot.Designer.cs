58fdc773e6f5db04c90ccdaba174d53b5506fa3a
CorbBauc <corbinmbaucom@gmail.com>
2025-12-15 17:07:15 -0600

    Created CardSlot UserControl for event driven controls so that I can register if a user clicks on a CardSlot in their hand. This includes three attributes: Card _card, bool _isSelected, and event Action<CardSlot> CardClicked.
    The constructor for CardSlot adds the controls to the CardSlot_Click event handler so that when they are clicked they trigger that event.
    
    the LoadCard method takes in a Card object and sets the card object passed to the CardSlot object's Card Object. This allows us to pass the selected card from the players hand to the CardSlot which then updates the CardSlot to show the Card from the players hand.
    
    The ClearSlot method sets the local card object to null and clears out the data in the CardSlot object.
    
    The SetSelected method sets the field _isSelected to true and then changes the BackColor of the CardSlot to gold.
    
    The ClearSelected method does the opposite of the SetSelected method with the BackColor set to transparent.

---
+++ CSC260-FinalProject-WinForm/CardSlot.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace CSC260_FinalProject_WinForm
+{
+    partial class CardSlot
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            labelMana = new Label();
+            labelHealth = new Label();
+            labelDamage = new Label();
+            pictureBoxCardArt = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxCardArt).BeginInit();
+            SuspendLayout();
+            // 
+            // labelMana
+            // 
+            labelMana.AutoSize = true;
+            labelMana.Location = new Point(64, 9);
+            labelMana.Name = "labelMana";
+            labelMana.Size = new Size(38, 15);
+            labelMana.TabIndex = 2;
+            labelMana.Text = "label1";
+            // 
+            // labelHealth
+            // 
+            labelHealth.AutoSize = true;
+            labelHealth.Location = new Point(13, 126);
+            labelHealth.Name = "labelHealth";
+            labelHealth.Size = new Size(38, 15);
+            labelHealth.TabIndex = 4;
+            labelHealth.Text = "label2";
+            // 
+            // labelDamage
+            // 
+            labelDamage.AutoSize = true;
+            labelDamage.Location = new Point(64, 126);
+            labelDamage.Name = "labelDamage";
+            labelDamage.Size = new Size(38, 15);
+            labelDamage.TabIndex = 6;
+            labelDamage.Text = "label3";
+            // 
+            // pictureBoxCardArt
+            // 
+            pictureBoxCardArt.Location = new Point(0, 0);
+            pictureBoxCardArt.Name = "pictureBoxCardArt";
+            pictureBoxCardArt.Size = new Size(115, 150);
+            pictureBoxCardArt.TabIndex = 7;
+            pictureBoxCardArt.TabStop = false;
+            // 
+            // CardSlot
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(labelDamage);
+            Controls.Add(labelHealth);
+            Controls.Add(labelMana);
+            Controls.Add(pictureBoxCardArt);
+            Name = "CardSlot";
+            Size = new Size(115, 150);
+            ((System.ComponentModel.ISupportInitialize)pictureBoxCardArt).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label labelMana;
+        private Label labelHealth;
+        private Label labelDamage;
+        private PictureBox pictureBoxCardArt;
+    }
+}
---
+++ CSC260-FinalProject-WinForm/CardSlot.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSC260_FinalProject_WinForm
+{
+    public partial class CardSlot : UserControl
+    {
+        private Card _card;
+        private bool _isSelected = false;
+        public event Action<CardSlot> CardClicked;
+
+        public CardSlot()
+        {
+            InitializeComponent();
+
+            this.Click += CardSlot_Click;
+            pictureBoxCardArt.Click += CardSlot_Click;
+            labelHealth.Click += CardSlot_Click;
+            labelMana.Click += CardSlot_Click;
+            labelDamage.Click += CardSlot_Click;
+
+        }
+
+        public Card Card
+        {
+            get {  return _card; }
+        }
+
+        private void CardSlot_Click(object? sender, EventArgs e)
+        {
+            if (_card != null)
+            {
+                CardClicked?.Invoke(this);
+            }
+        }
+
+        public void LoadCard(Card card)
+        {
+            _card = card;
+
+            if (card == null)
+            {
+                ClearSlot();
+                return;
+            }
+
+            pictureBoxCardArt.Image = card.Art;
+            labelHealth.Text = card.Health.ToString();
+            labelMana.Text = card.Mana.ToString();
+            labelDamage.Text = card.Damage.ToString();
+
+        }
+
+        public void ClearSlot()
+        {
+            _card = null;
+
+            pictureBoxCardArt.Image = null;
+            labelHealth.Text = "";
+            labelMana.Text = "";
+            labelDamage.Text = "";
+        }
+
+        public void SetSelected()
+        {
+            _isSelected = true;
+            this.BackColor = Color.Gold;
+        }
+
+        public void ClearSelected()
+        {
+            _isSelected = false;    
+            this.BackColor = Color.Transparent;
+
+        }
+    }
+}
